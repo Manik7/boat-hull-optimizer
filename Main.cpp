55228acbb1e3da991b0563728ae6539fb4aaba3f
Marco Nikander <m.nikander@tu-be.de>
2015-12-16 01:09:46 +0100

    tried to get the initialization running with a narrower beam, seems hopeless. You should perhaps try creating a station-factory or a hull-factory after all, which can rpoduce reasonable seed hulls

--- Main.cpp
+++ Main.cpp
@@ -4,26 +4,26 @@
 
 int main () {
 	OptimizableHull hull;
-	/*hull.generate_stations();
+	hull.generate_stations();
 	hull.compute_properties();
 	hull.print_hull();
 	hull.export_hull_coordinates("seed.dat");
-	std::cout << "random hull generated, satisfies_constraints() = " << hull.satisfies_constraints() << "\n\n\n";*/
+// 	std::cout << "random hull generated, satisfies_constraints() = " << hull.satisfies_constraints() << "\n\n\n";
 	
-	MonteCarloOptimizer<OptimizableHull> carlos(hull);
+// 	MonteCarloOptimizer<OptimizableHull> carlos(hull);
 	GreedyOptimizer<OptimizableHull> greed(hull);
 	
-	carlos.run(1000000);
-	hull.print_hull();
-	hull.export_hull_coordinates("carlos.dat");
+// 	carlos.run(1000000);
+// 	hull.print_hull();
+// 	hull.export_hull_coordinates("carlos.dat");
 	
 	if (hull.satisfies_constraints()) {
-		std::cout << "Carlos did it! Starting Greed...\n";		
+		std::cout << "Seed generated successfully! Starting Greed...\n";		
 		
 		greed.run(1000000);
 		hull.print_hull();
 		hull.export_hull_coordinates("greed.dat");
 	} else {
-		std::cout << "Carlos FAILED!\n";
+		std::cout << "Seeding FAILED!\n";
 	}
 }
