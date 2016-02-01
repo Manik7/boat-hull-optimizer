b92e8b0bd9f92c7d551c451872fa7400463560f6
Marco Nikander <m.nikander@tu-be.de>
2016-02-01 23:20:35 +0100

    did horrible things to the source code to add color

--- hull_plotter/hull_plotter/hullplotter.cpp
+++ hull_plotter/hull_plotter/hullplotter.cpp
@@ -54,9 +54,12 @@ void HullPlotter::body_plan(Hull_qt& hull, QPainter& painter) {
         transform.scale(-1.0, 1.0);
         painter.setTransform(transform);
 
+        painter.setPen(QColor(0,255,0));
         painter.drawLine(origin, beam);
+        painter.setPen(QColor(255,0,0));
         painter.drawLine(beam, chine);
         painter.drawLine(chine, keel);
+        painter.setPen(QColor(0,0,0));
         painter.drawLine(keel, origin);
 
     }
@@ -65,15 +68,17 @@ void HullPlotter::body_plan(Hull_qt& hull, QPainter& painter) {
 //looking from above
 void HullPlotter::breadth_plan(Hull_qt& hull, QPainter& painter) {
 
-    std::vector<QPointF> points;
+    std::vector<QPointF> chine_points;
     for (int i = 0; i<5; ++i) {
-        points.push_back(QPointF(hull.stations[i].chine.x, hull.stations[i].chine.z));
+        chine_points.push_back(QPointF(hull.stations[i].chine.x, hull.stations[i].chine.z));
     }
+    std::vector<QPointF> origin_points;
     for (int i = 4; i>=0; --i) {
-        points.push_back(QPointF(hull.stations[i].origin.x, hull.stations[i].origin.z));
+        origin_points.push_back(QPointF(hull.stations[i].origin.x, hull.stations[i].origin.z));
     }
+    std::vector<QPointF> beam_points;
     for (int i = 0; i<5; ++i) {
-        points.push_back(QPointF(hull.stations[i].beam.x, hull.stations[i].beam.z));
+        beam_points.push_back(QPointF(hull.stations[i].beam.x, hull.stations[i].beam.z));
     }
 
     QTransform transform;
@@ -82,31 +87,60 @@ void HullPlotter::breadth_plan(Hull_qt& hull, QPainter& painter) {
     transform.rotate(-90);
     painter.setTransform(transform);
 
-    for (auto first = ++(points.begin()), second = points.begin(); first!=points.end(); ++first, ++second) {
+    painter.setPen(QColor(0,0,255));
+    for (auto first = ++(chine_points.begin()), second = chine_points.begin(); first!=chine_points.end(); ++first, ++second) {
+        painter.drawLine(*first, *second);
+    }
+
+    painter.setPen(QColor(0,0,0));
+    for (auto first = ++(origin_points.begin()), second = origin_points.begin(); first!=origin_points.end(); ++first, ++second) {
+        painter.drawLine(*first, *second);
+    }
+
+    painter.setPen(QColor(0,255,0));
+    for (auto first = ++(beam_points.begin()), second = beam_points.begin(); first!=beam_points.end(); ++first, ++second) {
         painter.drawLine(*first, *second);
     }
 
     transform.scale(-1,1);
     painter.setTransform(transform);
 
-    for (auto first = ++(points.begin()), second = points.begin(); first!=points.end(); ++first, ++second) {
+    painter.setPen(QColor(0,0,255));
+    for (auto first = ++(chine_points.begin()), second = chine_points.begin(); first!=chine_points.end(); ++first, ++second) {
         painter.drawLine(*first, *second);
     }
+
+    painter.setPen(QColor(0,0,0));
+    for (auto first = ++(origin_points.begin()), second = origin_points.begin(); first!=origin_points.end(); ++first, ++second) {
+        painter.drawLine(*first, *second);
+    }
+
+    painter.setPen(QColor(0,255,0));
+    for (auto first = ++(beam_points.begin()), second = beam_points.begin(); first!=beam_points.end(); ++first, ++second) {
+        painter.drawLine(*first, *second);
+    }
+
+/*    for (auto first = ++(points.begin()), second = points.begin(); first!=points.end(); ++first, ++second) {
+        painter.drawLine(*first, *second);
+    }*/
 
     //TODO: Plot stations
 }
 
 //looking from the side
 void HullPlotter::sheer_plan(Hull_qt& hull, QPainter& painter) {
-    std::vector<QPointF> points;
+    painter.setPen(QColor(0,0,0));
+    std::vector<QPointF> chine_points;
     for (int i = 0; i<5; ++i) {
-        points.push_back(QPointF(hull.stations[i].chine.y, hull.stations[i].chine.z));
+        chine_points.push_back(QPointF(hull.stations[i].chine.y, hull.stations[i].chine.z));
     }
+    std::vector<QPointF> origin_points;
     for (int i = 4; i>=0; --i) {
-        points.push_back(QPointF(hull.stations[i].origin.y, hull.stations[i].origin.z));
+        origin_points.push_back(QPointF(hull.stations[i].origin.y, hull.stations[i].origin.z));
     }
+    std::vector<QPointF> keel_points;
     for (int i = 0; i<5; ++i) {
-        points.push_back(QPointF(hull.stations[i].keel.y, hull.stations[i].keel.z));
+        keel_points.push_back(QPointF(hull.stations[i].keel.y, hull.stations[i].keel.z));
     }
 
     QTransform transform;
@@ -115,9 +149,19 @@ void HullPlotter::sheer_plan(Hull_qt& hull, QPainter& painter) {
     transform.rotate(-90);
     painter.setTransform(transform);
 
-    for (auto first = ++(points.begin()), second = points.begin(); first!=points.end(); ++first, ++second) {
+    painter.setPen(QColor(0,0,255));
+    for (auto first = ++(chine_points.begin()), second = chine_points.begin(); first!=chine_points.end(); ++first, ++second) {
         painter.drawLine(*first, *second);
     }
+    painter.setPen(QColor(0,255,0));
+    for (auto first = ++(origin_points.begin()), second = origin_points.begin(); first!=origin_points.end(); ++first, ++second) {
+        painter.drawLine(*first, *second);
+    }
+    painter.setPen(QColor(0,0,0));
+    for (auto first = ++(keel_points.begin()), second = keel_points.begin(); first!=keel_points.end(); ++first, ++second) {
+        painter.drawLine(*first, *second);
+    }
+
 
     //TODO: Plot stations
 }
